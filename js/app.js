821b46d469bdb4bd5bec889ec9efbef74d17468f
crsepulvedach <31741474+crsepulvedach@users.noreply.github.com>
2019-05-06 18:37:34 -0400

    Add files via upload
    Primer commit

---
+++ js/app.js
@@ -0,0 +1,49 @@
+window.onload = function() {
+
+
+
+    // Timing and frames per second
+    var lastframe = 0;
+    var fpstime = 0;
+    var framecount = 0;
+    var fps = 0;
+
+    // Mouse dragging
+    var drag = false;
+// Level object
+  var level = {
+      x: 250,         // X position
+      y: 113,         // Y position
+      columns: 7,     // Number of tile columns
+      rows: 7,        // Number of tile rows
+      tilewidth: 40,  // Visual width of a tile
+      tileheight: 40, // Visual height of a tile
+      tiles: [],      // The two-dimensional tile array
+      selectedtile: { selected: false, column: 0, row: 0 }
+  };
+
+
+
+  var caramelos = [
+    "image\\1.png",
+  "image\\2.png",
+  "image\\3.png",
+  "image\\4.png"];
+
+  var columnas = $(".panel-tablero div");
+
+// Initialize the game
+function init() {
+// Initialize the two-dimensional tile array
+for (var i = 0; i < columnas.length; i++) {
+      for (var j = 0; j < 7; j++) {
+        var img = Math.floor(Math.random() * (4 - 1 + 1));
+        $(columnas[i]).append("<img src='" + caramelos[img] +
+          "' class='elemento' width='100px' height='100px'>");
+      }
+    }
+// (...)
+}
+
+init();
+}
