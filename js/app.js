c0c449860dd8d6a998bf122cd7f1ef60438c127f
crsepulvedach <31741474+crsepulvedach@users.noreply.github.com>
2019-05-07 17:37:11 -0400

    Add files via upload
    Commit 07/05

--- js/app.js
+++ js/app.js
@@ -34,9 +34,7 @@ window.onload = function() {
     $(".main-titulo").animate({color: '#DCFF0E'},1000);
   };
 
-  function cuentaatras(){
-    $('#timer').countdown({until: +300});
-  }
+
 
   function mostrarHora() {
     var fecha = new Date(), // nuevo objeto Fecha
@@ -49,6 +47,8 @@ window.onload = function() {
 // Initialize the game
   function init() {
     // Initialize the two-dimensional tile array
+
+
     for (var i = 0; i < columnas.length; i++) {
       for (var j = 0; j < 7; j++) {
         var img = Math.floor(Math.random() * (4 - 1 + 1));
@@ -58,9 +58,79 @@ window.onload = function() {
     }
   }
 
-init();
+  function tablero(){
+    $(".panel-tablero").hide(1800);
+    $(".time").hide(1800);
+    $(".panel-score").animate({width:"100%"});
+    $(".panel-score").prepend("<h2 style='text-align: center;' class='data-titulo title-fin'>Fin del Juego</h2>")
+  }
+
+  //Animación que mueve el balón de futbol
+ function shoot(elemento, posV, posH){
+   $(elemento)
+   .animate(
+     {
+       top: posV,
+       left: posH
+     },{
+       queue: false
+     },800)
+   .animate(
+     {
+       width: "-=70"
+     },
+     {
+       step: function(now, fx){
+         $(elemento).css("transform","rotate("+now*10+"deg)");
+       },
+       queue: false,
+       duration: 1800,
+       complete: function(){
+
+         var x= parseFloat(posH);
+         var y= parseFloat(posV);
+         var centro = parseFloat($("#arquero").css("left"))+235;
+         //Validaciones si pega en los palos
+           if ((x>=979&&x<=1009)&&(y>=88)) {
+             rebote(elemento,"derecha");
+           }else if ((y<=88&&y>=50)&&(x>=450)) {
+             rebote(elemento, "arriba");
+           }else if ((x>=454&&x<=486)&&(y>=88)) {
+             rebote(elemento, "izquierda");
+           }else if ((x<454||x>1009)||(y<55||y>409)) { //Validación si va por fuera
+             rebote(elemento, "lejos");
+           }else if(((x>(centro-55))&&(x<(centro+23)))&&(y>154&&y<236)){ //Validaciones si pega en el arquero
+             rebote(elemento, "arriba");
+           }else if(((x>(centro-128))&&(x<(centro+95)))&&(y>280&&y<362)){
+             rebote(elemento, "arriba");
+           }else if(((x>(centro-185))&&(x<(centro+143)))&&(y>226&&y<280)){
+             rebote(elemento, "arriba");
+           }else if(((x>(centro-122))&&(x<(centro-88)))&&(y>362&&y<432)){
+             rebote(elemento, "izquierda");
+           }else if (((x>(centro+63))&&(x<(centro+97)))&&(y>362&&y<432)) {
+             rebote(elemento, "derecha");
+           }else{
+             $(elemento).css("zIndex","3"); //Si no sucede nada de lo anterior se indica que se marco el Gol
+             $("#arquero").css("zIndex","4");
+             golScored(elemento);
+           }
+       }
+     }
+   )
+ }
+
+  //tablero();
+  //shoot($(".panel-tablero"), 100, 200);
+//Evento reiniciar el juego al presionar el botón
+  $(".btn-reinicio").on("click", function(){
+    init();
+    //startTimer();
+    run_clock('timer');
+  });
+//  shoot($("panel-tablero"), 400, 300);
+//init();
 
 //setInterval(mostrarHora, 1000); // la función "mostrarHora" se ejecuta cada segundo
 setInterval(titulo, 1500);
-setInterval(cuentaatras, 1000)
+
 }
